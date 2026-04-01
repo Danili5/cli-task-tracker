ebc4ab95a71a9825b07013c065802730cd87f16a
Danial <danazad2016@gmail.com>
2026-04-01 15:31:22 -0500

    implement-add-task-feature

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-import json , os
+import json
+import pathlib
 
-json_file = os.path.abspath("data.json")
+path = pathlib.Path(__file__).parent / "data.json"
+tasks = {}
 
+def read():
+    try:
+        with open(path, "r") as file_to_read:
+            return json.load(file_to_read)
+    except FileNotFoundError:
+        print("invalid file path", "creating a new empty json file: 'data.json'")
+        return {}
+
+def write():
+    with open(path, "w") as file_to_write:
+        json.dump(tasks, file_to_write, indent = 4)
+
+tasks = read()
+
+print("CLI Task Tracker")
+
+
+while True:
+    command = input("command: ").lower()
+
+    if command == "exit":
+        print("Exiting")
+        break
+    elif command == "show":
+        print("Tasks")
+        print(tasks)
+    elif command == "add":
+        print("New Task")
+
+        new_task = input("name: ")
+
+        if new_task == "":
+            continue
+        else:
+            tasks[new_task] = "in progress"
+            write()
+    else:
+        print("Did not understand the command. Try again.")
