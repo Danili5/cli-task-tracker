0ddee99b083d603ef07eb6f297cb71754b720be0
Danial <danazad2016@gmail.com>
2026-04-12 23:42:26 -0500

    implement-update-task-feature

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import pathlib
 class Logic:
     def __init__(self):
         self.path = pathlib.Path(__file__).parent / "data.json"
-        self.tasks = None
+        self.tasks = {}
 
         try:
             with open(self.path, "r") as file_to_read:
                 self.tasks = json.load(file_to_read)
         except FileNotFoundError:
             print("invalid file path", "creating a new empty json file: 'data.json'")
-            self.tasks = {}
 
     def add(self, task_name):
         self.tasks[task_name] = {"status": "in-progress"}
@@ -19,8 +18,27 @@ class Logic:
         self._id()
         self._write()
 
-    def update(self):
-        pass
+    def update(self, task_id): 
+        valid_update_inputs = ["d","i"]
+
+        for task_name, task_info in self.tasks.items():
+            if task_id == task_info["id"]:
+                print("Update Command Menu:\ndone\nin-progress\nback")
+
+                while True:
+                    status = input("Enter status: ")
+
+                    if any(command in status for command in valid_update_inputs):
+                            if status != task_info["status"]:
+                                task_info.update({"status": status})
+                                self._write()
+                                return
+                            else:
+                                print(f"the status of {task_name} is already {status}")
+                    else:
+                        print("task not found try again")
+            
+        print("task not found")
     
     def delete(self):
         pass
@@ -33,17 +51,15 @@ class Logic:
         with open(self.path, "w") as file_to_write:
             json.dump(self.tasks, file_to_write, indent = 4)
 
-    def _search_task(self, task_name_search):
-        for task_name in self.tasks.keys():
-            if task_name_search == task_name:
-                return task_name
-
-        return
+    def _search_task(self, task_id):
+        for task_name, task_info in self.tasks.items():
+            if task_id == task_info["id"]:
+                return True
 
 class CLI:
     def __init__(self):
         print("Welcome to Task Tracker CLI")
-        print("Command Menu\nadd\nupdate\ndelete\nexit")
+        print("Command Menu\nadd\nupdate\ndelete\nexit:")
 
         valid_inputs = ["add", "update", "remove", "exit"]
 
@@ -61,12 +77,12 @@ class CLI:
                 if any(command in command_input for command in ["add"]):
                     user_input = input("Enter the task name: ")
                 else:
-                    user_input = input("Enter the task ID: ")
+                    user_input = int(input("Enter the task ID: "))
                 
                 if command_input == "add":
                     logic.add(user_input)
                 elif command_input == "update":
-                    logic.update(logic._search_task(user_input))
+                    logic.update(user_input)
                 elif command_input == "exit":
                     break
             else:
@@ -81,17 +97,3 @@ cli = CLI()
 # List all tasks that are done
 # List all tasks that are not done
 # List all tasks that are in progress
-
-# path = pathlib.Path(__file__).parent / "data.json"
-# tasks = {}
-
-# def read():
-    
-
-# def write():
-#     with open(path, "w") as file_to_write:
-#         json.dump(tasks, file_to_write, indent = 4)
-
-# tasks = read()
-
-# print("CLI Task Tracker\ncommands:\nexit\nshow\nadd\nupdate")
